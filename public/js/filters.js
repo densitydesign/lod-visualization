3002e5122ba11197931a622554b8a701aab1a070
giorgiocaviglia <giorgio.caviglia@gmail.com>
2013-08-13 17:40:09 +0200

    added images

--- public/js/filters.js
+++ public/js/filters.js
@@ -2,9 +2,31 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', function (version) {
-    return function (text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  });
+angular.module('myApp.filters', [])
+
+ .filter('thumbnail', function () {
+		return function (images) {
+			if (!images.length) return "";
+			var filtered = images.filter(function(d){ return d.subtype == "xlarge"; });
+			if (!filtered.length) return "";
+			return "http://nytimes.com/" + filtered[0].url;
+		}
+	})
+
+ .filter('truncate', function () {
+        return function (text, length, end) {
+            if (isNaN(length))
+                length = 10;
+
+            if (end === undefined)
+                end = "...";
+
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length-end.length) + end;
+            }
+
+        };
+    });
